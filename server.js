63ea48e90dffbaf04fa83f00dccd35d876de6abd
Osho Rajneesh <oshorajneesh453@gmail.com>
2021-07-03 11:17:39 +0530

    initalized everything

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const expressLayout= require('express-ejs-layouts');
+const path = require('path');
+const PORT = process.env.PORT || 3000 // doosre system pr chalane ke liye.
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+//set template engine
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port xyz ${PORT}`);
+})
